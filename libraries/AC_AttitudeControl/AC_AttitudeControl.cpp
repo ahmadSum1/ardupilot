f5cbd1f8d1fc16f486991462e70c9cb9a3dd48e1
Sakib Ahmed Sumdany <ahmadSum1@users.noreply.github.com>
2019-03-04 20:25:12 +0600

    to solve "/libraries/AC_AttitudeControl/AC_AttitudeControl.h:7:0,                  from ../../libraries/AC_AttitudeControl/AC_AttitudeControl.cpp:1: ../../libraries/AC_AttitudeControl/AC_AttitudeControl.cpp:114:56: error: 'const class AC_AttitudeControl' has no member named '_ang_vel_roll_max'      AP_GROUPINFO("RATE_R_MAX", 17, AC_AttitudeControl, _ang_vel_roll_max, 0.0f),                                                         ^ ../../libraries/AP_Param/AP_Param.h:75:70: note: in definition of macro 'AP_CLASSTYPE'  #define AP_CLASSTYPE(clazz, element) ((uint8_t)(((const clazz *) 1)->element.vtype))                                                                       ^ ../../libraries/AP_Param/AP_Param.h:87:54: note: in expansion of macro 'AP_GROUPINFO_FLAGS'  #define AP_GROUPINFO(name, idx, clazz, element, def) AP_GROUPINFO_FLAGS(name, idx, clazz, element, def, 0)                                                       ^ ../../libraries/AC_AttitudeControl/AC_AttitudeControl.cpp:114:5: note: in expansion of macro 'AP_GROUPINFO'      AP_GROUPINFO("RATE_R_MAX", 17, AC_AttitudeControl, _ang_vel_roll_max, 0.0f),"

--- libraries/AC_AttitudeControl/AC_AttitudeControl.cpp
+++ libraries/AC_AttitudeControl/AC_AttitudeControl.cpp
@@ -103,36 +103,6 @@ const AP_Param::GroupInfo AC_AttitudeControl::var_info[] = {
     // @User: Advanced
     AP_GROUPINFO("ANG_LIM_TC", 16, AC_AttitudeControl, _angle_limit_tc, AC_ATTITUDE_CONTROL_ANGLE_LIMIT_TC_DEFAULT),
 
-    // @Param: RATE_R_MAX
-    // @DisplayName: Angular Velocity Max for Roll
-    // @Description: Maximum angular velocity in roll axis
-    // @Units: deg/s
-    // @Range: 0 1080
-    // @Increment: 1
-    // @Values: 0:Disabled, 360:Slow, 720:Medium, 1080:Fast
-    // @User: Advanced
-    AP_GROUPINFO("RATE_R_MAX", 17, AC_AttitudeControl, _ang_vel_roll_max, 0.0f),
-
-    // @Param: RATE_P_MAX
-    // @DisplayName: Angular Velocity Max for Pitch
-    // @Description: Maximum angular velocity in pitch axis
-    // @Units: deg/s
-    // @Range: 0 1080
-    // @Increment: 1
-    // @Values: 0:Disabled, 360:Slow, 720:Medium, 1080:Fast
-    // @User: Advanced
-    AP_GROUPINFO("RATE_P_MAX", 18, AC_AttitudeControl, _ang_vel_pitch_max, 0.0f),
-
-    // @Param: RATE_Y_MAX
-    // @DisplayName: Angular Velocity Max for Yaw
-    // @Description: Maximum angular velocity in yaw axis
-    // @Units: deg/s
-    // @Range: 0 1080
-    // @Increment: 1
-    // @Values: 0:Disabled, 360:Slow, 720:Medium, 1080:Fast
-    // @User: Advanced
-    AP_GROUPINFO("RATE_Y_MAX", 19, AC_AttitudeControl, _ang_vel_yaw_max, 0.0f),
-
     // @Param: INPUT_TC
     // @DisplayName: Attitude control input time constant (aka smoothing)
     // @Description: Attitude control input time constant.  Low numbers lead to sharper response, higher numbers to softer response
